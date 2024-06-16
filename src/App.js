e3aaa525cf0dbc6cb3df49c1a928cf464a765f5c
Cương Cao <103629423+CuongCao1603@users.noreply.github.com>
2024-06-17 00:27:48 +0700

    u

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-// import logo from "./logo.svg";
 import "./App.scss";
-// import AddUserInfor from "./components/AddUserInfor";
-// import { useDispatch, useSelector } from "react-redux";
-// import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-// import DisplayInfor from "./components/DisplayInfor";
-
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello CuongCao
-        <MyComponent />
-      </div>
-    );
-  }
+import Header from "./components/Header/Header";
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header/>
+    </div>
+  );
 }
 export default App;
